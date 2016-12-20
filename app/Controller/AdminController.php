7627f401b5a58fe0f09fc2af69d277804ae267c5
borislav <b.manolov@gmail.com>
2016-12-20 10:19:02 +0100

    implement edit locking for books

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 use App\Entity\Book;
 use App\Entity\User;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as EasyAdminController;
@@ -23,7 +24,7 @@ class AdminController extends EasyAdminController {
 	}
 
 	protected function prePersistBookEntity(Book $book) {
-		return $book->setCreatedBy($this->getUser()->getUsername());
+		return $book->setCreatedBy($this->getUsername());
 	}
 
 	protected function preUpdateBookEntity(Book $book) {
@@ -31,19 +32,39 @@ class AdminController extends EasyAdminController {
 		if ($diffs) {
 			$revision = $book->createRevision();
 			$revision->setDiffs($diffs);
-			$revision->setCreatedBy($this->getUser()->getUsername());
+			$revision->setCreatedBy($this->getUsername());
 			$this->em->persist($revision);
 		}
+		$book->clearLock();
 	}
 
 	protected function createBookEditForm(Book $book, $fields) {
 		$form = $this->createEditForm($book, $fields);
 		$this->bookPreEdit = clone $book;
+		if ($book->isLockedForUser($this->getUsername())) {
+			$form->addError(new FormError("В момента този запис се редактира от {$book->getLockedBy()}."));
+		} else {
+			$book->setLock($this->getUsername());
+			$this->em->persist($book);
+			$this->em->flush();
+		}
 		return $form;
+	}
+
+	protected function createBookEntityFormBuilder(Book $book, $view) {
+		$builder = $this->createEntityFormBuilder($book, $view);
+		if ($book->isLockedForUser($this->getUsername())) {
+			$builder->setDisabled(true);
+		}
+		return $builder;
 	}
 
 	/** @return User */
 	protected function getUser() {
 		return $this->get('security.token_storage')->getToken()->getUser();
+	}
+
+	protected function getUsername() {
+		return $this->getUser()->getUsername();
 	}
 }
--- app/Entity/Book.php
+++ app/Entity/Book.php
@@ -12,6 +12,8 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
  */
 class Book implements \JsonSerializable {
 
+	const LOCK_EXPIRE_TIME = 21600;
+
 	/**
 	 * @ORM\Column(type="integer")
 	 * @ORM\Id
@@ -454,6 +456,18 @@ class Book implements \JsonSerializable {
 	 * @ORM\Column(type="boolean")
 	 */
 	private $isIncomplete;
+
+	/**
+	 * @var \DateTime
+	 * @ORM\Column(type="datetime", nullable=true)
+	 */
+	private $lockedAt;
+
+	/**
+	 * @var string
+	 * @ORM\Column(type="string", length=50, nullable=true)
+	 */
+	private $lockedBy;
 
 //	/**
 //	 * @ORM\OneToMany(targetEntity="BookItem", mappedBy="book")
@@ -1224,6 +1238,28 @@ class Book implements \JsonSerializable {
 
 	public function setIsIncomplete($isIncomplete) {
 		$this->isIncomplete = $isIncomplete;
+	}
+
+	public function setLock($user) {
+		$this->lockedBy = $user;
+		$this->lockedAt = new \DateTime();
+	}
+
+	public function clearLock() {
+		$this->lockedBy = null;
+		$this->lockedAt = null;
+	}
+
+	public function isLockedForUser($user) {
+		return $this->lockedBy !== null && $this->lockedBy !== $user && !$this->isLockExpired();
+	}
+
+	public function isLockExpired() {
+		return $this->lockedAt === null || (time() - $this->lockedAt->getTimeStamp() > self::LOCK_EXPIRE_TIME);
+	}
+
+	public function getLockedBy() {
+		return $this->lockedBy;
 	}
 
 	/** @ORM\PrePersist */
