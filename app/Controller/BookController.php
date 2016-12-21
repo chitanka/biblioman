f1883d3109afa9a6d8711cd0c6ab0942641a0b40
borislav <b.manolov@gmail.com>
2016-12-21 21:57:08 +0100

    refactor book controller, move logic to repository

--- app/Controller/BookController.php
+++ app/Controller/BookController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Controller;
 
+use App\Entity\BookRepository;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 use Pagerfanta\Pagerfanta;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -10,41 +11,15 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class BookController extends Controller {
 
+	const ITEMS_PER_PAGE = 15;
+
 	/**
 	 * @Route("/", name="books")
 	 */
 	public function indexAction(Request $request) {
-		$page = $request->query->get('page', 1);
-		$maxResults = 15;
-		/* @var $queryBuilder \Doctrine\ORM\QueryBuilder */
-		$queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder()
-			->select('b')
-			->from('App:Book', 'b');
-		if ($searchQuery = $request->query->get('q')) {
-			if (is_numeric($searchQuery)) {
-				$queryBuilder
-					->where('b.pubDate = ?1')
-					->setParameter('1', $searchQuery);
-			} else if (preg_match('/(\d+)-(\d+)/', $searchQuery, $matches)) {
-				$queryBuilder
-					->where('b.pubDate BETWEEN ?1 AND ?2')
-					->setParameters([1 => $matches[1], 2 => $matches[2]]);
-			} else {
-				$queryBuilder
-					->where('b.title LIKE ?1')
-					->orWhere('b.subtitle LIKE ?1')
-					->orWhere('b.author LIKE ?1')
-					->orWhere('b.translator LIKE ?1')
-					->orWhere('b.compiler LIKE ?1')
-					->orWhere('b.editor LIKE ?1')
-					->orWhere('b.publisher LIKE ?1')
-					->setParameter('1', "%$searchQuery%");
-			}
-		}
-		$adapter = new DoctrineORMAdapter($queryBuilder);
-		$pager = new Pagerfanta($adapter);
-		$pager->setMaxPerPage($maxResults);
-		$pager->setCurrentPage($page);
+		$searchQuery = $request->query->get('q');
+		$adapter = new DoctrineORMAdapter($this->repo()->filterByQuery($searchQuery));
+		$pager = $this->pager($request, $adapter);
 		return $this->render('Book/index.html.twig', [
 			'pager' => $pager,
 			'fields' => $this->getParameter('book_fields_short'),
@@ -56,18 +31,8 @@ class BookController extends Controller {
 	 * @Route("/incomplete", name="books_incomplete")
 	 */
 	public function listIncompleteAction(Request $request) {
-		$page = $request->query->get('page', 1);
-		$maxResults = 15;
-		/* @var $queryBuilder \Doctrine\ORM\QueryBuilder */
-		$queryBuilder = $this->getDoctrine()->getManager()->createQueryBuilder()
-			->select('b')
-			->from('App:Book', 'b')
-			->where('b.isIncomplete = 1')
-			->orWhere('b.nbScans = 0');
-		$adapter = new DoctrineORMAdapter($queryBuilder);
-		$pager = new Pagerfanta($adapter);
-		$pager->setMaxPerPage($maxResults);
-		$pager->setCurrentPage($page);
+		$adapter = new DoctrineORMAdapter($this->repo()->filterIncomplete());
+		$pager = $this->pager($request, $adapter);
 		return $this->render('Book/listIncomplete.html.twig', [
 			'pager' => $pager,
 			'fields' => $this->getParameter('book_fields_short'),
@@ -78,9 +43,7 @@ class BookController extends Controller {
 	 * @Route("/search-duplicates", name="books_search_duplicates")
 	 */
 	public function searchDuplicatesAction(Request $request) {
-		$books = $this->getDoctrine()->getManager()
-			->getRepository('App:Book')
-			->findDuplicatesByTitle($request->query->get('title'), $request->query->get('id'));
+		$books = $this->repo()->findDuplicatesByTitle($request->query->get('title'), $request->query->get('id'));
 		return $this->render('Book/searchDuplicates.html.twig', [
 			'books' => $books,
 			'fields' => $this->getParameter('book_fields_short'),
@@ -91,9 +54,7 @@ class BookController extends Controller {
 	 * @Route("/{id}", name="books_show")
 	 */
 	public function showAction($id) {
-		$book = $this->getDoctrine()->getManager()
-			->getRepository('App:Book')
-			->find($id);
+		$book = $this->repo()->find($id);
 		if (!$book) {
 			throw $this->createNotFoundException('Book not found');
 		}
@@ -101,5 +62,17 @@ class BookController extends Controller {
 			'book' => $book,
 			'fields' => $this->getParameter('book_fields_long'),
 		]);
+	}
+
+	private function pager(Request $request, $adapter) {
+		$pager = new Pagerfanta($adapter);
+		$pager->setMaxPerPage(self::ITEMS_PER_PAGE);
+		$pager->setCurrentPage($request->query->get('page', 1));
+		return $pager;
+	}
+
+	/** @return BookRepository */
+	private function repo() {
+		return $this->getDoctrine()->getManager()->getRepository('App:Book');
 	}
 }
--- app/Entity/BookRepository.php
+++ app/Entity/BookRepository.php
@@ -1,6 +1,7 @@
 <?php namespace App\Entity;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
 
 class BookRepository extends EntityRepository {
 
@@ -18,5 +19,45 @@ class BookRepository extends EntityRepository {
 			->andWhere('b.title = ?2')->setParameter('2', $title)
 			->getQuery()
 			->getResult();
+	}
+
+	/**
+	 * @param string $query
+	 * @return QueryBuilder
+	 */
+	public function filterByQuery($query) {
+		$qb = $this->createQueryBuilder('b');
+		if (empty($query)) {
+			return $qb;
+		}
+		if (is_numeric($query)) {
+			$qb
+				->where('b.pubDate = ?1')
+				->setParameter('1', $query);
+		} else if (preg_match('/(\d+)-(\d+)/', $query, $matches)) {
+			$qb
+				->where('b.pubDate BETWEEN ?1 AND ?2')
+				->setParameters([1 => $matches[1], 2 => $matches[2]]);
+		} else {
+			$qb
+				->where('b.title LIKE ?1')
+				->orWhere('b.subtitle LIKE ?1')
+				->orWhere('b.author LIKE ?1')
+				->orWhere('b.translator LIKE ?1')
+				->orWhere('b.compiler LIKE ?1')
+				->orWhere('b.editor LIKE ?1')
+				->orWhere('b.publisher LIKE ?1')
+				->setParameter('1', "%$query%");
+		}
+		return $qb;
+	}
+
+	/**
+	 * @return QueryBuilder
+	 */
+	public function filterIncomplete() {
+		return $this->createQueryBuilder('b')
+			->where('b.isIncomplete = 1')
+			->orWhere('b.nbScans = 0');
 	}
 }
