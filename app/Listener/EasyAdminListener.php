aa9b01ba55ca12e2d496ec8557e872b2601ef66a
borislav <b.manolov@gmail.com>
2017-02-04 15:34:27 +0100

    add an EasyAdminListener - save unsaved new book covers

---
+++ app/Listener/EasyAdminListener.php
@@ -0,0 +1,31 @@
+<?php namespace App\Listener;
+
+use App\Entity\Book;
+use JavierEguiluz\Bundle\EasyAdminBundle\Event\EasyAdminEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+class EasyAdminListener implements EventSubscriberInterface {
+
+	public static function getSubscribedEvents() {
+		return [
+			EasyAdminEvents::POST_UPDATE => ['saveNewBookCovers'],
+		];
+	}
+
+	public function saveNewBookCovers(GenericEvent $event) {
+		$entity = $event->getSubject();
+
+		if (!($entity instanceof Book)) {
+			return;
+		}
+
+		$em = $event->getArgument('em');
+		foreach ($entity->getCovers() as $cover) {
+			if ($cover->isNew()) {
+				$em->persist($cover);
+			}
+		}
+		$em->flush();
+	}
+}
