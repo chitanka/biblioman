0ec528e0ccc232fb3a7518c8cce0f9281861fe6a
borislav <b.manolov@gmail.com>
2017-03-19 15:44:11 +0100

    remove array and Traversable param hint from EntityCollection::fromCollection()

--- app/Collection/EntityCollection.php
+++ app/Collection/EntityCollection.php
@@ -23,7 +23,7 @@ class EntityCollection extends ArrayCollection {
 	}
 
 	/**
-	 * @param static|Collection|array|\Traversable $collection
+	 * @param static|Collection $collection
 	 * @return static
 	 */
 	public static function fromCollection($collection) {
