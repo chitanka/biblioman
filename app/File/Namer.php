ba3f94602eeabc3c54dc8a6ee1c12d388799e50d
borislav <b.manolov@gmail.com>
2018-03-18 17:14:57 +0100

    prepend the entity ID to the uploaded files

--- app/File/Namer.php
+++ app/File/Namer.php
@@ -1,5 +1,6 @@
 <?php namespace App\File;
 
+use App\Entity\Entity;
 use Vich\UploaderBundle\Mapping\PropertyMapping;
 use Vich\UploaderBundle\Naming\Polyfill\FileExtensionTrait;
 
@@ -10,14 +11,14 @@ class Namer implements \Vich\UploaderBundle\Naming\NamerInterface {
 	/**
 	 * Creates a name for the file being uploaded.
 	 *
-	 * @param object $object The object the upload is attached to.
+	 * @param Entity $object The object the upload is attached to.
 	 * @param PropertyMapping $mapping The mapping to use to manipulate the given object.
 	 *
 	 * @return string The file name.
 	 */
 	public function name($object, PropertyMapping $mapping) {
 		$extension = $this->fixExtension($mapping->getFile($object));
-		return uniqid().'.'.$extension;
+		return $object->getId() .'-'. uniqid() .'.'. $extension;
 	}
 
 	private function fixExtension(\Symfony\Component\HttpFoundation\File\UploadedFile $file) {
